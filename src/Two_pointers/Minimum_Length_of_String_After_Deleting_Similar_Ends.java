3818c542eafba612f82b0af2f6dc2b5182fae72f
Aditya Singh <adityasingh3781@outlook.com>
2025-04-08 20:12:08 +0530

    Added problem Minimum_Length_of_String_After_Deleting_Similar_Ends

--- src/Two_pointers/Minimum_Length_of_String_After_Deleting_Similar_Ends.java
+++ src/Two_pointers/Minimum_Length_of_String_After_Deleting_Similar_Ends.java
@@ -6,5 +6,18 @@ public class Minimum_Length_of_String_After_Deleting_Similar_Ends {
         int left = 0;
         int right = s.length() - 1;
 
+        while (left < right && s.charAt(left) == s.charAt(right)) {
+            char currentChar = s.charAt(left);
+
+            while (left <= right && s.charAt(left) == currentChar) {
+                left++;
+            }
+
+            while (left <= right && s.charAt(right) == currentChar) {
+                right--;
+            }
+        }
+
+        return right - left + 1;
     }
 }
