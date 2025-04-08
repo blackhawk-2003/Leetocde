321c1194380282bdf26a4491d411f3b7d95ab028
Aditya Singh <adityasingh3781@outlook.com>
2025-04-08 19:55:57 +0530

    Added problem of the day

---
+++ src/Two_pointers/Minimum_Length_of_String_After_Deleting_Similar_Ends.java
@@ -0,0 +1,10 @@
+package Two_pointers;
+
+public class Minimum_Length_of_String_After_Deleting_Similar_Ends {
+
+    public int minimumLength(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+    }
+}
---
+++ src/Two_pointers/Minimum_Number_of_Operations_to_Make_Elements_in_Array_Distinct.java
@@ -0,0 +1,5 @@
+package Two_pointers;
+
+public class Minimum_Number_of_Operations_to_Make_Elements_in_Array_Distinct {
+
+}
